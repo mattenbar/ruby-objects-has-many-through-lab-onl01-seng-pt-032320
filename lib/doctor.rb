e17c6d94a73218c3a5bfa9892fc683b03b519558
matthew enbar <matthew.enbar@gmail.com>
2020-04-09 02:51:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,4 +20,8 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
 end
